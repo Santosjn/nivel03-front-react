bad3c2ccbadbdeb6e3b4de903005ad43a9d74915
Santosjn <jeansjcsp@gmail.com>
2020-06-06 20:10:29 -0300

    Adding navigation ro repository detail.

--- src/pages/Repository/index.tsx
+++ src/pages/Repository/index.tsx
@@ -1,7 +1,19 @@
 import React from 'react';
+import { useRouteMatch } from 'react-router-dom';
+
+interface RepositoryParams {
+  repository: string;
+}
 
 const Repository: React.FC = () => {
-  return <h1>Repository Page</h1>;
+  const { params } = useRouteMatch<RepositoryParams>();
+
+  return (
+    <h1>
+      Repository Page:
+      {params.repository}
+    </h1>
+  );
 };
 
 export default Repository;
